e1ed584c7bd9d43f2283a88c0e5a12937e2d6f03
alan <alan@wireless-tag.com>
2022-09-05 11:12:27 +0800

    gfx: support rotate

--- include/qmsd.h
+++ include/qmsd.h
@@ -3,9 +3,19 @@
 
 #include "cJSON.h"
 
-#define QMSD_VERSION	"2.0"
+#define QMSD_VERSION	"2.1"
+
+typedef enum {
+	QMSD_DISP_ORIGIN,
+	QMSD_DISP_90,
+	QMSD_DISP_180,
+	QMSD_DISP_270,
+} qmsd_disp_dir;
 
 void qmsd_init(void);
 void qmsd_deinit(void);
+
+void qmsd_disp_set_dir(qmsd_disp_dir dir);
+qmsd_disp_dir qmsd_disp_get_dir(void);
 
 #endif
